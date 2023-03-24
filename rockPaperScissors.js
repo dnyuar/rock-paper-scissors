ca299417c4e47a1f05c30dd7ff2f15cd64814b74
dnyuar <60218795+dnyuar@users.noreply.github.com>
2023-03-24 15:46:53 +0000

    creating getComputerChoice function

---
+++ rockPaperScissors.js
@@ -0,0 +1,23 @@
+// Rock paper scissors project 
+/*
+Function called ‘getComputerChoice’
+- Should return a random choice of either rock, paper or scissors
+
+Plan
+- create function ‘getComputerChoice’
+- create variable choice
+- store in that variable an array which contains the items rock, paper and scissors
+- choose a random item from that array and return it (console.log() first)
+- return the array item with the index number of the random variable
+
+Function that plays a single round of rock, paper, scissors
+Takes two parameters, playerSelection and computerSelection
+Returns a string of who won the round (e.g You won, rock beats scissors)
+*/
+
+function getComputerChoice() {
+    const choice = ["ROCK", "PAPER", "SCISSORS"]
+
+    const randomVariable = Math.floor(Math.random() * choice.length)
+    console.log(choice[randomVariable])
+}
